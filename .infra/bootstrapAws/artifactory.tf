bc399f0389d7eea9ce3d55f91d076401a60b042d
Nathan Wang <nathanbotinker@gmail.com>
2022-10-06 22:16:41 +1300

    Create makefile

--- .infra/bootstrapAws/artifactory.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "artifactory" {
-  bucket        = var.ARTIFACTORY_BUCKET_NAME
-  force_destroy = var.FORCE_DESTROY
-}
--- .infra/bootstrapAws/terraform_backend.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "terraform_backend" {
-  bucket        = var.TERRAFORM_BACKEND_BUCKET_NAME
-  force_destroy = var.FORCE_DESTROY
-}
